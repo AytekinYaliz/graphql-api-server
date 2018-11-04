427e9bc5c8562cc2a4b0ab2626b202137a8625c3
Aytekin Yaliz <aytekin.yaliz@dentsuaegis.com>
2018-11-04 02:19:39 +0000

    initials

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const customerController = require('./controllers/customerController')
+const productController = require('./controllers/productController')
+const orderController = require('./controllers/orderController')
+
+
+const app = express()
+
+app.get('/', (req, res, next) => {
+   res.send(`
+   <a href="/customers">Customers</a> <br />
+   <a href="/products">Products</a> <br />
+   <a href="/orders">Orders</a>
+   `)
+})
+
+
+/**
+ * Customers endpoints
+ */
+app.get('/customers', customerController.getAll)
+app.get('/customers/:id', customerController.getOne)
+
+/**
+ * Orders endpoints
+ */
+app.get('/orders', orderController.getAll)
+app.get('/orders/:id', orderController.getOne)
+
+/**
+ * Products endpoints
+ */
+app.get('/products', productController.getAll)
+app.get('/products/:id', productController.getOne)
+
+module.exports = app
---
+++ src/controllers/customerController.js
@@ -0,0 +1,20 @@
+const customerRepo = require('../repositories/customerRepository')
+
+function getAll(req, res, next) {
+   console.log( 'hit /customers' )
+
+   res.json( customerRepo.getAll() )
+}
+
+function getOne(req, res, next) {
+   console.log( 'hit /customer/:id' )
+
+   const id = req.params.id;
+
+   res.json( customerRepo.getOne(id) )
+}
+
+module.exports = {
+   getAll,
+   getOne
+}
